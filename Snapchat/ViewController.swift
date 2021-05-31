d09cfb37a8d36a841627e5561536e3b0c6df5cb4
Jose Cancino <jose.cancino@tecsup.edu.pe>
2021-05-31 16:49:44 -0500

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Mac 09 on 5/31/21.
+//  Copyright © 2021 Mac 09. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
